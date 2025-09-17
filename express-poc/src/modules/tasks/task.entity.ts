af3d2f06accd8a7606756b105d402ab2691b1dec
Tsplivalo <63412263+Tsplivalo@users.noreply.github.com>
2025-09-16 23:46:39 -0300

    implementación de estado en task

--- express-poc/src/modules/tasks/task.entity.ts
+++ express-poc/src/modules/tasks/task.entity.ts
@@ -3,6 +3,7 @@ export class Task {
     public title: string,
     public description: string,
     public idUser: number,
+    public estado: 'PENDIENTE' | 'COMPLETO',
     public id?: number,
   ) {}
 }
--- express-poc/src/modules/tasks/task.routes.ts
+++ express-poc/src/modules/tasks/task.routes.ts
@@ -7,6 +7,8 @@ import {
   createTask,
   updateTask,
   deleteTask,
+  validacionEstado,
+  completeTask,
 } from './task.controller.js';
 import { checkSchema } from 'express-validator';
 
@@ -16,5 +18,6 @@ taskRouter.get('/', getAllTasks);
 taskRouter.get('/:id', getOneTaskById);
 taskRouter.post('/', checkSchema(bodySchema), createTask);
 taskRouter.put('/:id', checkSchema(bodySchema), updateTask);
-taskRouter.patch('/:id', checkSchema(partBodySchema), updateTask);
+taskRouter.patch('/:id/complete', completeTask);
+taskRouter.patch('/:id', checkSchema(partBodySchema), validacionEstado ,updateTask);
 taskRouter.delete('/:id', deleteTask);
